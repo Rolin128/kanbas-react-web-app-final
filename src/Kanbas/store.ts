dc2f3f526592a5d5ef134dbac805f49860cb98e2
Yue (Claire) He <115207213+icecreamlovr@users.noreply.github.com>
2024-12-09 23:48:45 -0800

    Add Quiz Preview screen

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -3,16 +3,19 @@ import modulesReducer from "./Courses/Modules/reducer";
 import accountReducer from "./Account/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
 import enrollmentsReducer from "./Dashboard/reducer";
-import quizzesReducer from "./Courses/Quizzes/reducer"
+import quizzesReducer from "./Courses/Quizzes/reducer";
+import questionsReducer from "./Courses/Quizzes/QuestionsReducer";
+import answerReducer from "./Courses/Quizzes/AnswerReducer";
 
 const store = configureStore({
-    reducer: {
-        modulesReducer,
-        accountReducer,
-        assignmentsReducer,
-        enrollmentsReducer,
-        quizzesReducer,
-
-    },
+  reducer: {
+    modulesReducer,
+    accountReducer,
+    assignmentsReducer,
+    enrollmentsReducer,
+    quizzesReducer,
+    questionsReducer,
+    answerReducer,
+  },
 });
 export default store;
