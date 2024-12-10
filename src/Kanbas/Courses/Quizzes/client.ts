6d8b025fef88b6e49f8a387b02b28f882815c2e5
baozitong <zitongbao1215@gmail.com>
2024-12-10 01:40:27 -0800

    solve the conflict

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -135,7 +135,6 @@ export const updateQuestion = async (questionId: string, questionData: string) =
   return response.data;
 };
 
-
 // Delete a question
 export const deleteQuestion = async (questionId: string) => {
   const response = await axios.delete(`${QUESTIONS_API}/${questionId}`);
